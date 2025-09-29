38ef8dfcb1e96b1b2a5f92d355b5655a5edc1987
JAVI24-25 <201902218@est.umss.edu>
2025-09-29 17:06:54 -0400

    comit de prueba

--- app/boris/page.tsx
+++ app/boris/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <main >
-            <h1>Componente básico</h1>
+            <h1>ccComponente básico</h1>
             <p>Este es un componente de página simple en Next.js (app router).</p>
         </main>
     );
